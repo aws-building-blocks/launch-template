c80541bcf14fc3bcce0398b3b14c62b95576d2f4
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-25 02:28:46 +0100

    Bug fix?

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_launch_template" "template" {
   description            = var.template_description
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  user_data              = var.use_userdata ? file(var.userdata_script_path) : null
+  user_data              = var.use_userdata ? base64encode(file(var.userdata_script_path)) : null
   vpc_security_group_ids = var.vpc_security_group_ids
 
   iam_instance_profile {
